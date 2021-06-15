fc5ee3d6b3ee201cb018e99a706f1c9fc326d047
gergő szijártó <54061320+geri666@users.noreply.github.com>
2021-06-15 08:30:37 +0200

    Add files via upload

---
+++ login2.0.ps1
@@ -0,0 +1,16 @@
+﻿# login 2.0
+# created by gergö szijarto 2021
+
+cls
+
+# reads username and password
+$user = Read-Host "enter username"
+
+$password = Read-Host "enter password"
+
+if ($user -eq "admin" -and $password -eq "ThisIsAPassword"){ # if both match the criteria
+    echo "private mode entered 😎" # do something
+} else {
+    echo "ERROR: wrong details" # do something else
+}
+
